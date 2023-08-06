235a8630f48eeac92f91f92dbe8d1fecd3a7c508
samuelk14 <smho-rey14@hotmail.com>
2023-08-06 15:42:50 -0500

    cambio de codigo login por codigo mio y agregue la carpeta estilos y el archivo de estilos del login

--- index.php
+++ index.php
@@ -5,17 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="estilos.css">
+    <!-- <link rel="stylesheet" href="/reset.css"> -->
+    <link rel="stylesheet" href="../formato-de-recepcion/estilos/index.css">
 </head>
 <body>
-    <img src="https://icons.veryicon.com/png/o/internet--web/prejudice/user-128.png" alt="foto de perfil">
-    <p>Por favor, ingrese su usuario y contraseña</p>
-
+    <section>
+        <img src="https://icons.veryicon.com/png/o/internet--web/prejudice/user-128.png" alt="foto de perfil">
+        <p>Por favor, ingrese su usuario y contraseña</p>
         <form action="./conexion/login.php" method="POST">
-            <input type="text" name="Nusuario" id="" placeholder="Ingrese usuario" required="">
-            <input type="password" name="pass" id="" placeholder="Contraseña" required="">
-            <input type="submit" name="" id="" value="Iniciar Sesión">
+            <input type="text" placeholder="usuario" name="Nusuario" id="username">
+            <input type="password" placeholder="Ingresa contraseña" name="pass" id="password">
+            <input type="submit" value="INICIAR SESIÓN">
         </form>
-    
+        <a href="
+        /signUp.html">Sign Up</a>
+    </section>    
 </body>
 </html>
