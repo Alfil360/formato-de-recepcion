6cbef2d6d3ded552de06f04280caf9c09601fa04
samuelk14 <smho-rey14@hotmail.com>
2023-08-09 17:42:52 -0500

    Cracion de la pagina administrador.php, modificacion del archivo login.php, en el cual se cambio el manejo que se le daba a la respuesta, de la cual se esperaba que cuando encontrara un registro, es decir, un usuario registrado, diera como resultado un 1 en la fila, por lo cual se mostraba un mensaje de usuario registrado o en su defecto se procedia inmediatamente a la pagina de recpecion. Se cambio esto y se configuró para que cuando encontrara un usuario registrado devolviera el tipo de usuario y asi por medio de switch-case se procediera a la pagina correspondiente de acuerdo al tipo de usuario

---
+++ administrador.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administracion</title>
+    <!-- <link rel="stylesheet" href="/reset.css"> -->
+    <link rel="stylesheet" href="../formato-de-recepcion/estilos/index.css">
+</head>
+<body>
+    <section>
+        <img src="https://icons.veryicon.com/png/o/internet--web/prejudice/user-128.png" alt="foto de perfil">
+        <p>Hola, esta es al pagina de administracion</p>        
+    </section>    
+</body>
+</html>
--- conexion/login.php
+++ conexion/login.php
@@ -1,28 +1,43 @@
 <?php
-// Se incluye conexión a la base de datos
-include "conexion.php";
+    // Se incluye conexión a la base de datos
+    include "conexion.php";
 
-$usu = $_POST['Nusuario'];
-$clave = $_POST['pass'];
+    $usu = $_POST['Nusuario'];
+    $clave = $_POST['pass'];
 
-//consulta a la base de datos un usuario registrado
+    // Consulta a la base de datos un usuario registrado y obtiene el tipo de funcionario
+    $consulta = "SELECT tipofuncionario FROM funcionario WHERE usuario = '".$usu."' AND contraseña = '".$clave."'";
 
+    //echo $consulta;
 
-$consulta = "SELECT * FROM funcionario WHERE usuario= '".$usu."' AND contraseña ='".$clave."'";
-//echo $consulta;
+    $resultado = $conexion->query($consulta);
 
-$resultado =$conexion->query($consulta);
+    $numreg = $resultado->num_rows;
 
-$numreg = $resultado->num_rows;
+    if ($numreg == 1) {
+        // Obtiene el tipo de funcionario desde el resultado de la consulta
+        $fila = $resultado->fetch_assoc();
+        $tipoFuncionario = $fila['tipofuncionario'];
 
-if($numreg == 1){
-    header("location:../recepcion.php");
-}
-
-
-else{
-    header("location:../Error.php");
-}
-
-
+        // Redirige a la página correspondiente al tipo de funcionario
+        switch ($tipoFuncionario) {
+            case 'recepcion':
+                header("location:../recepcion.php");
+                break;
+            case 'bascula':
+                header("location:../bascula.php");
+                break;
+            case 'laboratorio':
+                header("location:../calidad.php");
+                break;
+            case 'administrador':
+                header("location:../administrador.php");
+                break;
+            default:
+                header("location:../Error.php");
+                break;
+        }
+    } else {
+        header("location:../Error.php");
+    }
 ?>
