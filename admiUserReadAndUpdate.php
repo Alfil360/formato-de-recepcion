190b09b47749097f94fb4dd416725bd309939f52
samuelk14 <smho-rey14@hotmail.com>
2023-08-31 09:18:54 -0500

    Creacion de la pagina crear usuario, se da la funcionalidad al boton flotante para que redirija a esta y se compelta el CRUD en usuarios administracion

--- admiUserReadAndUpdate.php
+++ admiUserReadAndUpdate.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Recepcion Form</title>
+    <title>Administracion de Usuario</title>
     <link rel="stylesheet" href="../formato-de-recepcion/estilos/form.css">
 </head>
 <body>
